de0f24ab8e49c8a885a427daae7bbd4b9e2cf130
Bukhosi Moyo <bukhosizimcode@gmail.com>
2021-07-22 22:14:10 +0200

    🔥 : Changed views to class based views

--- task/urls.py
+++ task/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.HomeListView, name="home"),
-    path('create/', views.TaskCreateView, name="create"),
-    path("update/<int:pk>/", views.TaskUpdateView, name="update"),
-    path("delete/<int:pk>/", views.TaskDeleteView, name="delete")
+    path('', views.TaskListView.as_view(), name="home"),
+    path('create/', views.TaskCreateView.as_view(), name="create"),
+    path("update/<int:pk>/", views.TaskUpdateView.as_view(), name="update"),
+    path("delete/<int:pk>/", views.TaskDeleteView.as_view(), name="delete")
 ]
--- task/views.py
+++ task/views.py
@@ -1,15 +1,41 @@
+from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 
-def HomeListView(request):
+class TaskListView(ListView):
+    template_name = "task/task_list.html"
+    model = Task
+    
+
+class TaskCreateView(CreateView):
+    template_name = "task/task_create.html"
+    form_class = TaskForm
+
+
+class TaskUpdateView(UpdateView):
+    form_class = TaskForm
+    template_name = "task/task_update.html"
+    queryset = Task.objects.all()
+    success_url = reverse_lazy("home")
+
+
+class TaskDeleteView(DeleteView):
+    template_name = "task/task_delete.html"
+    queryset = Task.objects.all()
+    success_url = reverse_lazy("home")
+    context_object_name = "item"
+
+
+"""def HomeListView(request):
     tasks = Task.objects.all()
     context = {"tasks":tasks}
     return render(request, "task/home.html", context)
+"""
 
-
-def TaskCreateView(request):
+"""def TaskCreateView(request):
     form = TaskForm()
 
     if request.method == 'POST':
@@ -19,9 +45,10 @@ def TaskCreateView(request):
         return redirect("/")
     
     context = {"form": form}
-    return render(request, "task/create.html", context)
+    return render(request, "task/create.html", context)"""
 
-def TaskUpdateView(request, pk):
+
+"""def TaskUpdateView(request, pk):
     tasks = Task.objects.get(id=pk)
     form = TaskForm(instance=tasks)
     if request.method == 'POST':
@@ -31,7 +58,10 @@ def TaskUpdateView(request, pk):
         return redirect("/")
     context = {"form":form, "task":tasks}
     return render(request, "task/update.html", context)
+"""
 
+
+"""
 def TaskDeleteView(request, pk):
     task = Task.objects.get(id=pk)
 
@@ -43,8 +73,4 @@ def TaskDeleteView(request, pk):
     context = {"item":task}
     return render(request, "task/delete.html", context)
 
-
-
-
-
-
+"""
